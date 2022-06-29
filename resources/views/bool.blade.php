11247422ae6f7b82793e96e237ded2ffc35966a0
Anastas Mironov <ast@l80.ru>
2022-06-29 18:59:17 +0900

    feat: add TD::macro bool()

---
+++ resources/views/bool.blade.php
@@ -0,0 +1,1 @@
+<span class="{{ $bool ? 'text-success' : 'text-danger' }}"><x-orchid-icon path="power" /></span>
--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -6,6 +6,7 @@ use AdminKit\Core\Console\InstallCommand;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Orchid\Platform\Dashboard;
+use Orchid\Screen\TD;
 
 class CoreServiceProvider extends ServiceProvider
 {
@@ -17,10 +18,23 @@ class CoreServiceProvider extends ServiceProvider
                 InstallCommand::class,
             ]);
         }
+
+        TD::macro('bool', function () {
+            $column = $this->column;
+            $this->render(function ($datum) use ($column) {
+                return view('admin-kit::bool', [
+                    'bool' => $datum->$column
+                ]);
+            });
+
+            return $this;
+        });
     }
 
     public function boot()
     {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'admin-kit');
+
         Route::domain((string)config('platform.domain'))
             ->prefix(Dashboard::prefix('/'))
             ->middleware(config('platform.middleware.private'))
