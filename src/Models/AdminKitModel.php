a6e5aef39160c3c3f1aac7997ebb413cb6b10a83
Anastas Mironov <ast@l80.ru>
2023-01-15 18:18:46 +0600

    feat: add AdminKitModel with cyrillic json chars

---
+++ src/Models/AdminKitModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AdminKit\Core\Models;
+
+class AdminKitModel
+{
+    /**
+     * Save to database cyrillic characters
+     * @param $value
+     * @return false|string
+     */
+    public function asJson($value)
+    {
+        return json_encode($value, JSON_UNESCAPED_UNICODE);
+    }
+
+}
