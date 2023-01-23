eb04d2c610eb2a7077333c4ff043d22c0e6787b3
Anastas Mironov <ast@l80.ru>
2023-01-23 15:21:26 +0600

    chore: add Directories config

--- config/admin-kit.php
+++ config/admin-kit.php
@@ -3,4 +3,11 @@
 // config for AdminKit/Core
 return [
     'locales' => ['ru', 'en'],
+
+    'packages' => [
+        'directories' => [
+            // Значения по умолчанию
+            // https://github.com/admin-kit/directories/blob/1.x/config/directories.php
+        ],
+    ],
 ];
