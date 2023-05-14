979d2f32f595b303a5d36adc85a3904549658ffd
Anastas Mironov <ast@l80.ru>
2023-05-15 01:14:14 +0600

    chore: update version number to v2.0.2

--- src/Core.php
+++ src/Core.php
@@ -4,29 +4,5 @@ namespace AdminKit\Core;
 
 class Core
 {
-    public const VERSION = '1.2.10';
-
-    /**
-     * Return source path
-     */
-    public function srcPath(): string
-    {
-        return __DIR__;
-    }
-
-    /**
-     * Return source path
-     */
-    public function shipPath(): string
-    {
-        return __DIR__.'/Ship';
-    }
-
-    /**
-     * Return source path
-     */
-    public function containersPath(): string
-    {
-        return __DIR__.'/Containers';
-    }
+    public const VERSION = '2.0.2';
 }
