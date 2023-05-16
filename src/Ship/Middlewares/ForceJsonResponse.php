6dd17c83113f45271f3b759123012d248bb7b35b
Anastas Mironov <ast@l80.ru>
2023-05-16 18:59:27 +0600

    feat: add forceJsonResponse middleware

---
+++ src/Ship/Middlewares/ForceJsonResponse.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AdminKit\Core\Ship\Middlewares;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ForceJsonResponse
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+        return $next($request);
+    }
+}
