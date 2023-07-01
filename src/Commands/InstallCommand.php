d7a369920a2f0ebd22890779e7061133bb746dfb
ast21 <ast21@users.noreply.github.com>
2023-07-01 11:05:39 +0000

    Fix styling

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -25,7 +25,7 @@ class InstallCommand extends Command
             ]);
             $this->call('vendor:publish', [
                 '--provider' => 'Spatie\MediaLibrary\MediaLibraryServiceProvider',
-                '--tag' => "migrations",
+                '--tag' => 'migrations',
             ]);
             $this->call('vendor:publish', [
                 '--provider' => CoreServiceProvider::class,
