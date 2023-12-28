7b6deb0c6b8188e3f1df37d283c605b3add7267f
NursultanIITU <nursultaniitu@gmail.com>
2023-12-28 17:49:16 +0600

    fix: add value of text in textarea

--- resources/views/forms/components/ckeditor-field.blade.php
+++ resources/views/forms/components/ckeditor-field.blade.php
@@ -7,7 +7,7 @@
     $rows = $getRows();
     $shouldAutosize = $shouldAutosize();
     $statePath = $getStatePath();
-
+    $fieldName = substr($statePath, strpos($statePath, ".") + 1);
     $initialHeight = (($rows ?? 2) * 1.5) + 0.75;
 @endphp
 
@@ -33,49 +33,48 @@
                 ->class(['fi-fo-textarea overflow-hidden'])
         "
     >
-        <div wire:ignore>
+        <div x-data="{ state: $wire.$entangle('name') }" wire:ignore>
             <textarea
-                id="editor"
-{{--                wire:model.lazy="{{$statePath}}"--}}
-                @if ($shouldAutosize)
-                    @if (FilamentView::hasSpaMode())
-                        ax-load="visible"
-                @else
-                    ax-load
-                @endif
-                ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('textarea', 'filament/forms') }}"
-                x-data="textareaFormComponent({ initialHeight: @js($initialHeight) })"
-                x-ignore
-                x-intersect.once="render()"
-                x-on:input="render()"
-                x-on:resize.window="render()"
+                    id="editor"
+                    @if ($shouldAutosize)
+                        @if (FilamentView::hasSpaMode())
+                            ax-load="visible"
+                    @else
+                        ax-load
+                    @endif
+                    ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('textarea', 'filament/forms') }}"
+                    x-data="textareaFormComponent({ initialHeight: @js($initialHeight) })"
+                    x-ignore
+                    x-intersect.once="render()"
+                    x-on:input="render()"
+                    x-on:resize.window="render()"
                 {{ $getExtraAlpineAttributeBag() }}
-                @endif
-                {{
-                    $getExtraInputAttributeBag()
-                        ->merge([
-                            'autocomplete' => $getAutocomplete(),
-                            'autofocus' => $isAutofocused(),
-                            'cols' => $getCols(),
-                            'disabled' => $isDisabled,
-                            'id' => $getId(),
-                            'maxlength' => (! $isConcealed) ? $getMaxLength() : null,
-                            'minlength' => (! $isConcealed) ? $getMinLength() : null,
-                            'placeholder' => $getPlaceholder(),
-                            'readonly' => $isReadOnly(),
-                            'required' => $isRequired() && (! $isConcealed),
-                            'rows' => $rows,
-                            $applyStateBindingModifiers('wire:model') => $statePath,
-                        ], escape: false)
-                        ->class([
-                            'block w-full border-none bg-transparent px-3 py-1.5 text-base text-gray-950 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6',
-                            'resize-none' => $shouldAutosize,
-                        ])
-                        ->style([
-                            "height: {$initialHeight}rem" => $shouldAutosize,
-                        ])
-                }}
-        ></textarea>
+                    @endif
+                    {{
+                        $getExtraInputAttributeBag()
+                            ->merge([
+                                'autocomplete' => $getAutocomplete(),
+                                'autofocus' => $isAutofocused(),
+                                'cols' => $getCols(),
+                                'disabled' => $isDisabled,
+                                'id' => $getId(),
+                                'maxlength' => (! $isConcealed) ? $getMaxLength() : null,
+                                'minlength' => (! $isConcealed) ? $getMinLength() : null,
+                                'placeholder' => $getPlaceholder(),
+                                'readonly' => $isReadOnly(),
+                                'required' => $isRequired() && (! $isConcealed),
+                                'rows' => $rows,
+                                $applyStateBindingModifiers('wire:model') => $statePath,
+                            ], escape: false)
+                            ->class([
+                                'block w-full border-none bg-transparent px-3 py-1.5 text-base text-gray-950 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6',
+                                'resize-none' => $shouldAutosize,
+                            ])
+                            ->style([
+                                "height: {$initialHeight}rem" => $shouldAutosize,
+                            ])
+                    }}
+        >    {{ $getRecord()->$fieldName }}</textarea>
         </div>
     </x-filament::input.wrapper>
 </x-dynamic-component>
@@ -86,6 +85,7 @@
 <script>
     CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
         // extraPlugins: [CKFinder],
+        // plugins: [CKFinder],
         toolbar: {
             items: [
                 'undo', 'redo',
