3b339b336b55642e169bc35a6bc90a8c734cdb72
Anastas Mironov <ast@l80.ru>
2024-03-25 09:55:41 +0700

    fix:(users): add create action

--- src/UI/Filament/Resources/UserResource/Pages/ListUsers.php
+++ src/UI/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -4,10 +4,18 @@ namespace AdminKit\Core\UI\Filament\Resources\UserResource\Pages;
 
 use AdminKit\Core\UI\Filament\Resources\UserResource;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Actions;
 
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 
     public function getTitle(): string
     {
