43568b64a90f87fbc0931b27d0b4837a105c1c94
Anastas Mironov <ast@l80.ru>
2024-07-04 14:39:06 +0900

    feat: add GithubClient

---
+++ src/DTO/Github/Permissions.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdminKit\Core\DTO\Github;
+
+use Spatie\LaravelData\Data;
+
+class Permissions extends Data
+{
+    public function __construct(
+        public bool $admin,
+        public bool $maintain,
+        public bool $push,
+        public bool $triage,
+        public bool $pull,
+    ) {
+    }
+}
