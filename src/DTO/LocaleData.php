8a0088fd3a5144bc150ed62f26a1f1585d4986f0
Anastas Mironov <ast@l80.ru>
2024-07-31 14:21:49 +0900

    feat: add LocaleData make/makeCollection static methods

--- src/DTO/LocaleData.php
+++ src/DTO/LocaleData.php
@@ -4,18 +4,31 @@ declare(strict_types=1);
 
 namespace AdminKit\Core\DTO;
 
+use AdminKit\Core\Facades\AdminKit;
+use Illuminate\Support\Collection;
 use Spatie\LaravelData\Data;
 
 class LocaleData extends Data
 {
-    public string $title;
+    public ?string $title;
 
-    public string $native;
+    public ?string $native;
 
     public function __construct(
         public string $code,
     ) {
         $this->title = __("admin-kit::language.$this->code");
         $this->native = __(key: "admin-kit::language.$this->code", locale: $this->code);
+    }
+
+    public static function make(): self
+    {
+        return new self(app()->getLocale());
+    }
+
+    public static function makeCollection(): Collection
+    {
+        return collect(AdminKit::locales())
+            ->map(fn (string $code) => new self($code));
     }
 }
