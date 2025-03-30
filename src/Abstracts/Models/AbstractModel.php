3674134d91c56270d7f18b600be704705b0c6d28
Anastas Mironov <anastas.mironov@freedompay.kz>
2025-03-30 17:51:36 +0500

    feat: add activiylog to admin-kit

--- src/Abstracts/Models/AbstractModel.php
+++ src/Abstracts/Models/AbstractModel.php
@@ -5,9 +5,18 @@ namespace AdminKit\Core\Abstracts\Models;
 use AdminKit\Core\Traits\CyrillicChars;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as LaravelEloquentModel;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 abstract class AbstractModel extends LaravelEloquentModel
 {
     use CyrillicChars;
     use HasFactory;
+    use LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logAll();
+    }
 }
